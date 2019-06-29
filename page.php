4ae7c350a8f35aa6a07fbcfc852d834080dca5ea
Johanne <johanne_lewis@hotmail.com>
2019-06-29 15:50:37 +0100

    Use wordpress function to only show breadcrumbs if page has a parent page

--- page.php
+++ page.php
@@ -17,10 +17,12 @@
 
   		<div class="container container--narrow page-section">
 
-    		<div class="metabox metabox--position-up metabox--with-home-link">
-      			<p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
-    		</div>
-    
+  			<?php if (wp_get_post_parent_id(get_the_ID()) ) : ?>
+    			<div class="metabox metabox--position-up metabox--with-home-link">
+      				<p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+    			</div>
+            <?php endif; ?>
+
     	<!--	<div class="page-links">
       			<h2 class="page-links__title"><a href="#">About Us</a></h2>
       			<ul class="min-list">
