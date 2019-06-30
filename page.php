581812f444c7cee16d1cc8ec3e32caa3a6b82a1d
Johanne <johanne_lewis@hotmail.com>
2019-06-30 12:22:47 +0100

    Uncomment sidebar menu

--- page.php
+++ page.php
@@ -23,13 +23,13 @@
     			</div>
             <?php endif; ?>
 
-    	<!--	<div class="page-links">
+    	<div class="page-links">
       			<h2 class="page-links__title"><a href="#">About Us</a></h2>
       			<ul class="min-list">
         			<li class="current_page_item"><a href="#">Our History</a></li>
         			<li><a href="#">Our Goals</a></li>
       			</ul>
-    		</div> -->
+    	</div>
 
     		<div class="generic-content">
       			<p><?php the_content(); ?></p>
