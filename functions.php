9fe11315fc349289e27cb24d4130d77fbf8056ab
Johanne <johanne_lewis@hotmail.com>
2019-06-30 21:53:17 +0100

    Add option to create two footer menu links or navigation items from wordress dashboard

--- functions.php
+++ functions.php
@@ -12,6 +12,8 @@
 
 	function fictional_university_features(){
 		register_nav_menu('headerMenuLocation', 'Header Menu Location');
+		register_nav_menu('footerLocationOne', 'Footer Location One');
+		register_nav_menu('footerLocationTwo', 'Footer Location Two');
 		add_theme_support('title-tag');
 	}
 
