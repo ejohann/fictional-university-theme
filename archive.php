dcf8936a2da070e47c2e39f19fdb7d90f2915fa7
Johanne <johanne_lewis@hotmail.com>
2019-07-01 10:46:43 +0100

    Use wordpress function to show title for all archive pages

--- archive.php
+++ archive.php
@@ -5,14 +5,16 @@
     		<div class="page-banner__content container container--narrow">
       			<h1 class="page-banner__title">
               <?php 
-                  if(is_category()) 
+                 /* if(is_category()) 
                    {
                      single_cat_title();
                    }
                  if(is_author())
                    {
                       echo "Posts by "; the_author();
-                   }
+                   } */
+                 the_archive_title();
+
               ?>
               
              </h1>
