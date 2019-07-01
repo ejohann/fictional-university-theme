f71eb3d0a72d2c231f5e1e956f7667cc8409e891
Johanne <johanne_lewis@hotmail.com>
2019-07-01 09:53:25 +0100

    Use wordpress function to show formatted date

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 				 <div class="post-item">
         			<h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         			<div class="metabox">
- 						<p>Post by <?php the_author_posts_link(); ?> on Date in Category</p>
+ 						<p>Post by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in Category</p>
         			</div>
         			<div class="generic-content">
         				<?php the_excerpt(); ?>
