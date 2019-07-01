d449d27157a4296ca6f16d2136e3df14be1a5307
Johanne <johanne_lewis@hotmail.com>
2019-07-01 10:49:58 +0100

    Use wordpress function to show description for all archive pages

--- archive.php
+++ archive.php
@@ -14,12 +14,11 @@
                       echo "Posts by "; the_author();
                    } */
                  the_archive_title();
-
               ?>
               
              </h1>
       			<div class="page-banner__intro">
-        			<p>Keep up with our latest news.</p>
+        			<p><?php the_archive_description(); ?></p>
       			</div>
     		</div>  
   		</div>
