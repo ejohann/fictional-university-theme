24b79b6c24d194b992649b4f5b05277a9cbe0840
Johanne <johanne_lewis@hotmail.com>
2019-07-02 18:52:43 +0100

    Create image size for page banner images

--- functions.php
+++ functions.php
@@ -18,6 +18,8 @@
 		add_theme_support('post-thumbnails');
 		add_image_size('professorLandscape', 400, 260, true);
 		add_image_size('professorPortrait', 480, 650, true);
+		add_image_size('pageBanner', 1500, 350, true);
+
 	}
 
 	add_action('after_setup_theme', 'fictional_university_features');
