596e606b954409fe64dbadc5547f306b5a88886d
Johanne <johanne_lewis@hotmail.com>
2019-07-02 12:33:47 +0100

    Create function to display all programs post on page ordered in alphabetical order

--- functions.php
+++ functions.php
@@ -21,6 +21,14 @@
 
 
 	function university_adjust_queries($query){
+
+		if(!is_admin() && is_post_type_archive('program') && $query->is_main_query())
+		 {
+		 	$query->set('orderby', 'title');
+		 	$query->set('order', 'ASC');
+		 	$query->set('posts_per_page', -1);
+		 }
+
 		if(!is_admin() && is_post_type_archive('event') && $query->is_main_query())
 		  {
 		  	$today = date('Ymd');
