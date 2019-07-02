6e574b2cab309b9d6960619ce5db20a0eb79738e
Johanne <johanne_lewis@hotmail.com>
2019-07-02 11:49:25 +0100

    Implement pagination system

--- page-past-events.php
+++ page-past-events.php
@@ -16,7 +16,7 @@
   			<?php 
           $today = date('Ymd');
           $pastEvents = new WP_Query(array(
-            'posts_per_page' => 1,
+            'paged' => get_query_var('paged', 1),
             'post_type' => 'event',
             'meta_key' => 'event_date',
             'orderby' => 'meta_value_num',
@@ -48,7 +48,9 @@
               </div>
             </div> 		
 			<?php } 
-				echo paginate_links();
+				echo paginate_links(array(
+          'total' => $pastEvents->max_num_pages
+        ));
         wp_reset_postdata();
 			?>
   		</div>
