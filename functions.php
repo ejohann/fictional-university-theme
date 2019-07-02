b893b2473a4a88cdaa8849943c4c02617cf20bbb
Johanne <johanne_lewis@hotmail.com>
2019-07-02 19:42:00 +0100

    Create a function for displaying a page banner

--- functions.php
+++ functions.php
@@ -1,4 +1,24 @@
 <?php 
+
+
+	function pageBanner()
+	 {
+	 	//php logic will live here
+	 	?>
+	 	<div class="page-banner">
+    		<div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner']; ?>);"></div>
+    		<div class="page-banner__content container container--narrow">
+      			<h1 class="page-banner__title"><?php the_title(); ?></h1>
+      			<div class="page-banner__intro">
+        			<p><?php the_field('page_banner_subtitle'); ?></p>
+      			</div>
+    		</div>  
+  		</div>
+
+        <?php
+	 }
+
+
   	
 	function fictional_university_files(){
 		wp_enqueue_script('university_main_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
