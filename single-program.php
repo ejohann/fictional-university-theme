17c2aaf5c0676d1bc62ba71581572e63dfae3ebe
Johanne <johanne_lewis@hotmail.com>
2019-07-02 17:39:24 +0100

    Use custom css class for unordered list items

--- single-program.php
+++ single-program.php
@@ -41,7 +41,7 @@
           if($relatedProfessors->have_posts()){
             echo "<hr class='section-break'>";
             echo "<h2 class='headline headline--medium'>" .get_the_title() .  " Professors</h2>";
-            echo "<ul class='link-list min-list'>";
+            echo "<ul class='professor-cards'>";
             while($relatedProfessors->have_posts()){
               $relatedProfessors->the_post(); ?>
                <li class="professor-card__list-item">
