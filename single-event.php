6840ca79ba11618bf77eaeda725673e8f765ac39
Johanne <johanne_lewis@hotmail.com>
2019-07-02 13:01:41 +0100

    Apply styles to related programs, and only show related programs if event has a related programs

--- single-event.php
+++ single-event.php
@@ -20,16 +20,19 @@
   			<div class="generic-content">
   				<?php the_content(); ?>
   			</div>
-        <ul>
-          <?php 
-              $relatedPrograms = get_field('related_programs');  
 
+        <?php $relatedPrograms = get_field('related_programs'); ?>
+        <?php if($relatedPrograms) : ?>
+          <hr class="section-break">
+          <h2 class="headline headline--medium">Related Program(s)</h2>
+          <ul class="link-list min-list">
+            <?php 
               foreach($relatedPrograms as $program)
                 { ?>      
                   <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
-          <?php }
-           ?>
+            <?php } ?>
         </ul>
+         <?php endif; ?>
   		</div>
         
 	<?php }
