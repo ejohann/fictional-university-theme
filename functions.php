761bece580b5fc7e4338a6597eb8a1398abf4db4
Johanne <johanne_lewis@hotmail.com>
2019-07-02 20:22:07 +0100

    Make banner function accept arguments with fallback to make it more dynamic and flexible

--- functions.php
+++ functions.php
@@ -1,16 +1,26 @@
 <?php 
 
 
-	function pageBanner()
+	function pageBanner($args)
 	 {
 	 	//php logic will live here
+	 	if(!$args['title'])
+	 	  {
+	 	  	$args['title'] = get_the_title();
+	 	  }
+
+	 	if(!$args['subtitle'])
+	 	  {
+	 	  	$args['subtitle'] = get_field('page_banner_subtitle');
+	 	  }
+
 	 	?>
 	 	<div class="page-banner">
     		<div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner']; ?>);"></div>
     		<div class="page-banner__content container container--narrow">
-      			<h1 class="page-banner__title"><?php the_title(); ?></h1>
+      			<h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
       			<div class="page-banner__intro">
-        			<p><?php the_field('page_banner_subtitle'); ?></p>
+        			<p><?php echo $args['subtitle'];; ?></p>
       			</div>
     		</div>  
   		</div>
