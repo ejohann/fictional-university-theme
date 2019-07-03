e66d39e46ac14aa426f45cd8e78076b4042e1053
Johanne <johanne_lewis@hotmail.com>
2019-07-03 10:40:40 +0100

    Create div for map object

--- single-campus.php
+++ single-campus.php
@@ -11,9 +11,10 @@
             <div class="metabox metabox--position-up metabox--with-home-link">
               <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('campus'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Campuses </a> <span class="metabox__main"><?php the_title(); ?></span></p>
           </div>
-  			<div class="generic-content">
-  				<?php the_content(); ?>
-  			</div>
+  			<div class="generic-content"><?php the_content(); ?></div>
+
+
+        <div class="acf-map"></div>
 
         <?php 
 
