6867809da281a42855257cb5037cf7c681f8c176
Johanne <johanne_lewis@hotmail.com>
2019-07-03 15:41:09 +0100

    Require file with function for customizing queries

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 	require('inc/page-banner.php');
 	require('inc/fictional-university-files.php');
   	require('inc/fictional-university-features.php');
+  	require('inc/university-adjust-queries.php');
 	
 
   function universityMapKey($api)
