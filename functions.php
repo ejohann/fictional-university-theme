dea64dcb3d1a80bcefe5eaf312a4c7988c646be4
Johanne <johanne_lewis@hotmail.com>
2019-07-03 16:15:43 +0100

    Removed code for increasing file upload size

--- functions.php
+++ functions.php
@@ -1,11 +1,5 @@
 <?php 
 
-
-	@ini_set( 'upload_max_size' , '64M' );
-	@ini_set( 'post_max_size', '64M');
-	@ini_set( 'max_execution_time', '300' );
-	@ini_set( 'max_input_time', '300' );
-
 	require('inc/page-banner.php');
 	require('inc/fictional-university-files.php');
   	require('inc/fictional-university-features.php');
