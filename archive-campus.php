5a85c3c2ef5e96b0af21f8f7ce7aea524bc6f68d
Johanne <johanne_lewis@hotmail.com>
2019-07-03 10:52:35 +0100

    Delete unnecessary code

--- archive-campus.php
+++ archive-campus.php
@@ -11,15 +11,12 @@
         <?php 
           while(have_posts()){the_post(); $mapLocation = get_field('map_location'); ?>
         
-          <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>">  
-            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h3>
+          <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>"> <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h3>
             <?php echo $mapLocation['address']; ?>
           </div>      
 
-			  <?php } 
-				      echo paginate_links();
-			     ?>
-        </div>
-  		</div>
+			  <?php }   ?>
+      </div>
+  	</div>
 
 <?php get_footer(); ?>
