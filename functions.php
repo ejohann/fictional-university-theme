24b8a16c42eabc7e56ee769423363931e0398f4d
Johanne <johanne_lewis@hotmail.com>
2019-07-03 16:03:57 +0100

    Increase wordpress file upload size and execution time

--- functions.php
+++ functions.php
@@ -1,13 +1,14 @@
 <?php 
 
 
+	@ini_set( 'upload_max_size' , '64M' );
+	@ini_set( 'post_max_size', '64M');
+	@ini_set( 'max_execution_time', '300' );
+	@ini_set( 'max_input_time', '300' );
+
 	require('inc/page-banner.php');
 	require('inc/fictional-university-files.php');
   	require('inc/fictional-university-features.php');
   	require('inc/university-adjust-queries.php');
   	require('inc/university-map-key.php');
-	
-
-
-
 ?>
