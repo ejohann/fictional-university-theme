c7f54ad0e1e0b7f7467547106a3fcd68604541c8
Johanne <johanne_lewis@hotmail.com>
2019-07-03 10:34:40 +0100

    Show title and address of location, and link address to campus page

--- archive-campus.php
+++ archive-campus.php
@@ -6,18 +6,17 @@
     ));
 ?>
 
-  		<div class="container container--narrow page-section">
-        <ul class="link-list min-list">
-          <div class="acf-map">
-  			     <?php while(have_posts()){
-				        the_post(); 
-                $mapLocation = get_field('map_location'); 
-            ?>
-            <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>">  </div>
-			       
-             <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-             
-			     <?php } 
+	  <div class="container container--narrow page-section">
+      <div class="acf-map">
+        <?php 
+          while(have_posts()){the_post(); $mapLocation = get_field('map_location'); ?>
+        
+          <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>">  
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h3>
+            <?php echo $mapLocation['address']; ?>
+          </div>      
+
+			  <?php } 
 				      echo paginate_links();
 			     ?>
         </div>
