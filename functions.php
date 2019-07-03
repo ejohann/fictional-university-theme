748deaca4276ad93d5010a3da029ecaeaef2bd58
Johanne <johanne_lewis@hotmail.com>
2019-07-03 15:50:15 +0100

    Require file with google api key

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
 	require('inc/fictional-university-files.php');
   	require('inc/fictional-university-features.php');
   	require('inc/university-adjust-queries.php');
+  	require('inc/university-map-key.php');
 	
 
 
