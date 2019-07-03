30cadb894239e95594021fe2a407421f05203605
Johanne <johanne_lewis@hotmail.com>
2019-07-03 15:48:18 +0100

    Delete code with google api key

--- functions.php
+++ functions.php
@@ -7,13 +7,6 @@
   	require('inc/university-adjust-queries.php');
 	
 
-  function universityMapKey($api)
-	 {
-	 	$api['key'] = 'AIzaSyAOhZXAOb_iMbEU-gxlxTtDPIq9Yyy_2No';
-	 	return $api;
-	 }
-
-	add_filter('acf/fields/google_map/api', 'universityMapKey');
 
 
 ?>
