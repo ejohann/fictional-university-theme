c3633a1f3c6e618995e967be57f6a897fedcd6aa
Johanne <johanne_lewis@hotmail.com>
2019-07-03 18:29:15 +0100

    Create skeleton for Search class, and use jquery library to open and close search overlay

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,8 +1,30 @@
-class Search{
+import $ from 'jquery';
 
+class Search{
+	// 1. Describe and create/initiate object
 	constructor(){
-		alert("I am a search");
+		this.openButton = $(".js-search-trigger");
+		this.closeButton = $(".search-overlay__close");
+		this.searchOverlay = $(".search-overlay");
+		this.events();
 	}
-}
+
+ 	// 2. events
+ 	events(){
+ 		this.openButton.on("click", this.openOverlay.bind(this));
+ 		this.closeButton.on("click", this.closeOverlay.bind(this));
+ 	}
+
+ 	//3. methods (function, action..)
+ 	openOverlay(){
+ 		this.searchOverlay.addClass("search-overlay--active");
+
+ 	}// close openOpenlay
+
+ 	closeOverlay(){
+ 		this.searchOverlay.removeClass("search-overlay--active");
+ 	}// close closeOverlay
+
+} // close Search class
 
 export default Search;
