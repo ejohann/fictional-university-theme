04beb5b9678c561463b5b5ff6722aca427b254ae
Johanne <johanne_lewis@hotmail.com>
2019-07-04 10:29:21 +0100

    Logic to display message if no results found

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -53,10 +53,13 @@ class Search{
  		$.getJSON('http://localhost/wp/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
  			this.resultsDiv.html(`
  				<h2 class="search-overlay__section-title">Search Results</h2>
- 				<ul class="link-list min-list">
- 					${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')} 
- 				</ul>
- 				`);
+ 				${posts.length ? '<ul class="link-list min-list">' : '<p>No search results found</p>'}
+ 				
+ 				 ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')} 
+ 				
+ 				${posts.length ? '</ul>' : '' }
+ 				
+ 			 `);
  			this.isSpinnerVisible = false;
  		});
  	}
