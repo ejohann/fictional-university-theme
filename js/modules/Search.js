8114db0bf8413a9df525a2ca28469ccc056029a1
Johanne <johanne_lewis@hotmail.com>
2019-07-04 06:59:40 +0100

    Add functionality to not do a search if the search field was cleared or is emtpy

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -28,12 +28,23 @@ class Search{
  		if(this.searchField.val() != this.previousValue)
  		  {
  		  	clearTimeout(this.typingTimer);
- 			if(!this.isSpinnerVisible)
- 		  	 {
- 		  		this.resultsDiv.html('<div class="spinner-loader"></div>');
- 		  		this.isSpinnerVisible = true;
- 		     }
- 			this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+
+ 		  	if(this.searchField.val())
+ 		  	  {
+ 		  	  	if(!this.isSpinnerVisible)
+ 		  	 	  {
+ 		  			this.resultsDiv.html('<div class="spinner-loader"></div>');
+ 		  			this.isSpinnerVisible = true;
+ 		     	  }
+ 				this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+ 		  	  }	
+ 		  	 else
+ 		  	  {
+ 		  	  	this.resultsDiv.html('');
+ 		  	  	this.isSpinnerVisible = false;
+ 		  	  }
+
+ 			
  		  }
  		this.previousValue = this.searchField.val();
  	}
