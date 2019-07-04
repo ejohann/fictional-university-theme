6a91847ba9c1a644497354613d96b6ad71f9049b
Johanne <johanne_lewis@hotmail.com>
2019-07-04 23:44:44 +0100

    Only get related professors if program query has results

--- inc/search-route.php
+++ inc/search-route.php
@@ -76,35 +76,40 @@
 
 		}
 
-		$programsMetaQuery = array('relation' => 'OR');
-
-		foreach($results['programs'] as $item)
-		  { 
-		  	array_push($programsMetaQuery, 	array(
+        // if program has results get related professors
+		if($results['programs'])
+		  {
+		  	 $programsMetaQuery = array('relation' => 'OR');
+             foreach($results['programs'] as $item)
+		      { 
+		     	array_push($programsMetaQuery, 	array(
 					'key' => 'related_programs',
 					'compare' => 'LIKE',
 					'value' => '"'.$item['id'].'"'
 				));
-		  }
+		      }
 
-		$programRelationshipQuery = new WP_Query(array(
-			'post_type' => 'professor',
-			'meta_query' => $programsMetaQuery
-		));
+		     $programRelationshipQuery = new WP_Query(array(
+				'post_type' => 'professor',
+				'meta_query' => $programsMetaQuery
+			 ));
 
-		while($programRelationshipQuery->have_posts()){
-			$programRelationshipQuery->the_post();
-				if(get_post_type() == 'professor'){
-				array_push($results['professors'], array(
-			 		'title' => get_the_title(),
-			 		'permalink' => get_the_permalink(),
-			 		'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
-			));
-			}
-		}
-
-		// remove duplicates
-		$results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR)); 
+			 while($programRelationshipQuery->have_posts())
+			   {
+			 	 $programRelationshipQuery->the_post();
+			 	 if(get_post_type() == 'professor')
+				  {
+				 	array_push($results['professors'], array(
+			 			'title' => get_the_title(),
+			 			'permalink' => get_the_permalink(),
+			 			'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+					));
+				 } // end if
+		      } // end while
+			// remove duplicates
+			$results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR)); 
+		  } // end if program has results
+		
 
 		return $results;
 	}
