c1ee6053cae0256b06848dacaae19512eed68e16
Johanne <johanne_lewis@hotmail.com>
2019-07-04 06:54:46 +0100

    Add functionality to only run search method, when the value in the search field changes

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -11,6 +11,7 @@ class Search{
 		this.events();
 		this.isOverlayOpen = false;
 		this.isSpinnerVisible = false;
+		this.previousValue;
 		this.typingTimer;
 	}
 
@@ -19,18 +20,22 @@ class Search{
  		this.openButton.on("click", this.openOverlay.bind(this));
  		this.closeButton.on("click", this.closeOverlay.bind(this));
  		$(document).on("keydown", this.keyPressDispatcher.bind(this));
- 		this.searchField.on("keydown", this.typingLogic.bind(this));
+ 		this.searchField.on("keyup", this.typingLogic.bind(this));
  	}
 
  	//3. methods (function, action..)
  	typingLogic(){
- 		clearTimeout(this.typingTimer);
- 		if(!this.isSpinnerVisible)
+ 		if(this.searchField.val() != this.previousValue)
  		  {
- 		  	this.resultsDiv.html('<div class="spinner-loader"></div>');
- 		  	this.isSpinnerVisible = true;
+ 		  	clearTimeout(this.typingTimer);
+ 			if(!this.isSpinnerVisible)
+ 		  	 {
+ 		  		this.resultsDiv.html('<div class="spinner-loader"></div>');
+ 		  		this.isSpinnerVisible = true;
+ 		     }
+ 			this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
  		  }
- 		this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+ 		this.previousValue = this.searchField.val();
  	}
 
  	getResults(){
