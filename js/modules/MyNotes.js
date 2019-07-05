640866568fed36e29fd88ea15e4047e7101377d1
Johanne <johanne_lewis@hotmail.com>
2019-07-05 21:26:11 +0100

    Create function for updating post

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -11,6 +11,7 @@ class MyNotes{
 	events(){
 	  $(".delete-note").on("click", this.deleteNote);
 	  $(".edit-note").on("click", this.editNote.bind(this));
+	   $(".update-note").on("click", this.updateNote.bind(this));
 	}
 
 	//3. methods
@@ -33,6 +34,34 @@ class MyNotes{
  		}
  	});
  }
+
+
+ updateNote(e){
+ 	var thisNote = $(e.target).parents("li");
+ 	var ourUpdatedPost = {
+ 		'title': thisNote.find(".note-title-field").val(),
+ 		'content': thisNote.find(".note-body-field").val()
+ 	}
+
+ 	$.ajax({
+ 		beforeSend: (xhr) => {
+ 			xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+ 		},
+ 		url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+ 		type: 'POST',
+ 		data: ourUpdatedPost,
+ 		success: (response) => {
+ 			this.makeNoteReadOnly(thisNote);
+ 			console.log("congrats");
+ 			console.log(response);
+ 		},
+ 		error: (response) => {
+ 			console.log("Sorry");
+ 			console.log(response);
+ 		}
+ 	});
+ }
+
 
  editNote(e){
  	var thisNote = $(e.target).parents("li");
