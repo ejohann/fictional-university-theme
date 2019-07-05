64fa9bbb3195c0825047a1d60bda771ce3eebb97
Johanne <johanne_lewis@hotmail.com>
2019-07-05 09:51:57 +0100

    Apply styles to search form

--- page-search.php
+++ page-search.php
@@ -45,9 +45,12 @@
     		<?php endif; ?>
 
     		<div class="generic-content">
-      			<form method="get" action="<?php echo esc_url(site_url('/')); ?>"> 
-              <input type="search" name="s">
-              <input type="submit" value="Search">
+      			<form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>"> 
+              <label class="headline headline--medium" for="s">Perform a Search: </label>
+              <div class="search-form-row">
+                  <input class="s" id="s" type="search" name="s" placeholder="What are you looking for?">
+                  <input class="search-submit" type="submit" value="Search">
+              </div>
             </form>
     		</div>
   		</div>
