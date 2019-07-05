af4fd629ee2c62a2b1807ab50973b5e652907751
Johanne <johanne_lewis@hotmail.com>
2019-07-05 17:55:43 +0100

    Create markup for my notes page

---
+++ page-my-notes.php
@@ -0,0 +1,20 @@
+<?php 
+	
+	get_header();
+
+	while(have_posts()){
+		the_post(); 
+    pageBanner();
+    ?>
+		
+
+
+  		<div class="container container--narrow page-section">
+
+  		
+  		</div>
+
+	<?php }
+
+	get_footer();
+?>
