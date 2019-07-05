50680f66f32ad3876bef653d9a46e6ea96456f6a
Johanne <johanne_lewis@hotmail.com>
2019-07-05 19:54:44 +0100

    Create functionality for deleting a post

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -14,7 +14,21 @@ class MyNotes{
 
 	//3. methods
  deleteNote(){
- 	alert("You clicked delete");
+ 	$.ajax({
+ 		beforeSend: (xhr) => {
+ 			xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+ 		},
+ 		url: universityData.root_url + '/wp-json/wp/v2/note/98',
+ 		type: 'DELETE',
+ 		success: (response) => {
+ 			console.log("congrats");
+ 			console.log(response);
+ 		},
+ 		error: (response) => {
+ 			console.log("Sorry");
+ 			console.log(response);
+ 		}
+ 	});
  }
 
 }
