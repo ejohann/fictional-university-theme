356c1343429ace3902ee922bc097af49d77c83f2
Johanne <johanne_lewis@hotmail.com>
2019-07-05 20:31:58 +0100

    Delete note by the id clicked on

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -13,12 +13,13 @@ class MyNotes{
 	}
 
 	//3. methods
- deleteNote(){
+ deleteNote(e){
+ 	var thisNote = $(e.target).parents("li");
  	$.ajax({
  		beforeSend: (xhr) => {
  			xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
  		},
- 		url: universityData.root_url + '/wp-json/wp/v2/note/98',
+ 		url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
  		type: 'DELETE',
  		success: (response) => {
  			console.log("congrats");
