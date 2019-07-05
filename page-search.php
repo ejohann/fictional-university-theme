30dcb0fde2758637bd415e0868cb561e54972058
Johanne <johanne_lewis@hotmail.com>
2019-07-05 10:42:18 +0100

    Delete code for search form

--- page-search.php
+++ page-search.php
@@ -45,13 +45,7 @@
     		<?php endif; ?>
 
     		<div class="generic-content">
-      			<form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>"> 
-              <label class="headline headline--medium" for="s">Perform a Search: </label>
-              <div class="search-form-row">
-                  <input class="s" id="s" type="search" name="s" placeholder="What are you looking for?">
-                  <input class="search-submit" type="submit" value="Search">
-              </div>
-            </form>
+      			
     		</div>
   		</div>
 
