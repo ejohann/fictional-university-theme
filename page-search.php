551cf376c06eb8f6507be26119c4427b67a320b6
Johanne <johanne_lewis@hotmail.com>
2019-07-05 09:33:42 +0100

    Submit form form as a get request instead of post request

--- page-search.php
+++ page-search.php
@@ -45,7 +45,7 @@
     		<?php endif; ?>
 
     		<div class="generic-content">
-      			<form action="<?php echo esc_url(site_url('/')); ?>"> 
+      			<form method="get" action="<?php echo esc_url(site_url('/')); ?>"> 
               <input type="search" name="s">
               <input type="submit" value="Search">
             </form>
