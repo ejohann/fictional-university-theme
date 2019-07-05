c0346071aa667b44e3ba50e2830c0c0a8c230a10
Johanne <johanne_lewis@hotmail.com>
2019-07-05 14:34:56 +0100

    Create function for changing the wordpress logo on login screen, and to load the css file for customizing the stlyes

--- inc/customize-login-screen.php
+++ inc/customize-login-screen.php
@@ -7,5 +7,18 @@
 	 	return esc_url(site_url('/'));
 	 }
 
+	 add_action('login_enqueue_scripts', 'ourLoginCSS');
 
+	 function ourLoginCSS()
+	  {
+	  	wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
+	  }
+
+	  // Customize Login Screen Logo
+	  function ourLoginLogo()
+		{
+  			return get_bloginfo('name');
+        }
+ 
+      add_filter('login_headertitle', 'ourLoginLogo');
 ?>
