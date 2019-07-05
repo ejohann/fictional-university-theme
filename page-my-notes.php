5e76d1be2c3408184fa72171047617c10ea100e2
Johanne <johanne_lewis@hotmail.com>
2019-07-05 19:00:36 +0100

    Show notes for current logged in user

--- page-my-notes.php
+++ page-my-notes.php
@@ -16,12 +16,30 @@
 
   		<div class="container container--narrow page-section">
 
+          <ul class="min-list link-list" id="my-notes">
+            <?php 
+              $userNotes = new WP_Query(array(
+                'post_type' => 'note',
+                'posts_per_page' => -1,
+                'author' => get_current_user_id()
+              ));
+              while($userNotes->have_posts())
+                {
+                  $userNotes->the_post(); ?>
+                    <li>
+                      <input class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+                      <textarea class="note-body-field" ><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+                    </li>
+
+                <?php }
+            ?>
+          </ul>
   		
   		</div>
 
 	<?php }
 
   endif;
-  
+
 	get_footer();
 ?>
