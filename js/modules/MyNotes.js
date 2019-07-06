1d30cb8de02d5612737e4a5949fe1eb419039947
Johanne <johanne_lewis@hotmail.com>
2019-07-06 09:53:44 +0100

    Change note status to private when creating

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -69,7 +69,7 @@ class MyNotes{
  	var ourNewPost = {
  		'title': $(".new-note-title").val(),
  		'content': $(".new-note-body").val(),
- 		'status': 'publish'
+ 		'status': 'private'
  	}
 
  	$.ajax({
