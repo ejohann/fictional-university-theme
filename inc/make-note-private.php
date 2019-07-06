bc007d75bf590ed7d7f396fec908065762ca2ac9
Johanne <johanne_lewis@hotmail.com>
2019-07-06 15:03:16 +0100

    Allow a user to create a maximum of 5 notes

--- inc/make-note-private.php
+++ inc/make-note-private.php
@@ -1,11 +1,16 @@
 <?php 
 
-	add_filter('wp_insert_post_data', 'makeNotePrivate');
+	add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
 
-	function makeNotePrivate($data){
+	function makeNotePrivate($data, $postarr){
 
 		if($data['post_type'] == 'note')
 		  {
+		  	if(count_user_posts(get_current_user_id(), 'note') > 4 && !$postarr['ID'])
+		  	  {
+		  	  	die('You have reached your note limit');
+		  	  }
+
 		  	$data['post_content'] =sanitize_textarea_field($data['post_content']);
 		  	$data['post_title'] =sanitize_text_field($data['post_title']);
 		  }
