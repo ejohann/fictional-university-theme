fc22ab8899ba6aeb2a34318a39bca971b6155a51
Johanne <johanne_lewis@hotmail.com>
2019-07-08 11:14:31 +0100

    Require file like route file

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php 
 
-	require('inc/search-route.php');
-	require('inc/university-custom-rest.php');
-	require('inc/page-banner.php');
-	require('inc/fictional-university-files.php');
+	  require('inc/like-route.php');
+    require('inc/search-route.php');
+	  require('inc/university-custom-rest.php');
+	  require('inc/page-banner.php');
+	  require('inc/fictional-university-files.php');
   	require('inc/fictional-university-features.php');
   	require('inc/university-adjust-queries.php');
   	require('inc/university-map-key.php');
