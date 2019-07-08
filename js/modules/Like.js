b2a64564c8c62c56d5160c45ea48a715a33803a8
Johanne <johanne_lewis@hotmail.com>
2019-07-08 14:08:59 +0100

    Fix bug in code

--- js/modules/Like.js
+++ js/modules/Like.js
@@ -57,9 +57,6 @@ class Like{
 			url: universityData.root_url + '/wp-json/university/v1/manageLike',
 			data: {'like': currentLikeBox.attr('data-like')},
 			type: 'DELETE',
-			data: {
-				'professorId' : currentLikeBox.data('professor')
-			},
 			success: (response) => {
 				currentLikeBox.attr('data-exists', 'no');
 				var likeCount = parseInt(currentLikeBox.find(".like-count").html(), 10);
