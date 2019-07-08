6d40b25d5eee097a87fda102c80c218ea0cae007
Johanne <johanne_lewis@hotmail.com>
2019-07-08 11:03:54 +0100

    Crete event listeners, with methods for creating and deleting a like - alerts a message for testing

--- js/modules/Like.js
+++ js/modules/Like.js
@@ -4,18 +4,36 @@ class Like{
 
 	//1. Constructor
 	constructor(){
-
+		this.events();
 	}
 
 
 	//2. Events
 	events(){
+	 $(".like-box").on("click", this.ourClickDispatcher.bind(this));
+	}
 
+ 
+
+	//3. Methods
+	ourClickDispatcher(e){
+		var currentLikeBox = $(e.target).closest(".like-box");
+		if(currentLikeBox.data('exists') == 'yes'){
+			this.deleteLike();
+		}
+		else{
+			this.createLike();
+		}
 	}
 
 
+	createLike(){
+		alert("create this like");
+	}
 
-	//3. Methods
+	deleteLike(){
+		alert("Delete this like");
+	}
 }
 
 
