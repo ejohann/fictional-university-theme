66773c714fb5e6553754c1590a2955af80df9082
Johanne <johanne_lewis@hotmail.com>
2019-07-09 11:21:14 +0100

    Delete unnecessary code

--- front-page.php
+++ front-page.php
@@ -96,8 +96,6 @@
             $homepageSlides->the_post(); 
 
             $slider_image = get_field('slider_image');
-
-            print($slider_image);
             $image_url = $slider_image['url'];
             $slider_link = get_field('slide_link');
             ?>
@@ -115,25 +113,6 @@
           <?php }
           wp_reset_postdata();
         ?>
- 
- <!-- <div class="hero-slider__slide" style="background-image: url(<?php //echo get_theme_file_uri('images/apples.jpg'); ?>);">
-    <div class="hero-slider__interior container">
-      <div class="hero-slider__overlay">
-        <h2 class="headline headline--medium t-center">An Apple a Day</h2>
-        <p class="t-center">Our dentistry program recommends eating apples.</p>
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-      </div>
-    </div>
-  </div>
-  <div class="hero-slider__slide" style="background-image: url(<?php //echo get_theme_file_uri('images/bread.jpg'); ?>);">
-    <div class="hero-slider__interior container">
-      <div class="hero-slider__overlay">
-        <h2 class="headline headline--medium t-center">Free Food</h2>
-        <p class="t-center">Fictional University offers lunch plans for those in need.</p>
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-      </div>
-    </div>
-  </div> -->
 </div>
 
 
